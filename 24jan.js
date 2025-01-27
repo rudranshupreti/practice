d9024890e7229ac8dca5638a627b7fa77fb2631c
rudranshupreti17@gmail.com <rudranshupreti7@gmail.com>
2025-01-27 11:36:57 +0530

    inital commit

---
+++ 24jan.js
@@ -0,0 +1,52 @@
+// Q.1 Write a function named minutesToHours that receives a number of minutes as parameter and returns a number representing the same amount of time but in hours.
+// const minutestoHours = (minutes) => {
+//     return minutes/60;
+// }
+// console.log(minutestoHours(60));
+
+// window.alert("hello");
+// window.print('hello');
+// document.write('hello');
+
+{/* <button onclick ="document.getElementById('hello').style.color='red'">Click me</button> */}
+
+// document.getElementById('hello').innerHTML = "Hello World";
+// document.getElementById('hello').style.color = "red";
+// let a = 19;
+// let b = 2+840;
+// console.log(a+b);
+// document.getElementById('hello').innerHTML = "hello sir";
+// document.getElementById('per').innerHTML = "kese hoo sir";
+// document.getElementById('per').style.background = "red";
+
+// var a = 20; 
+// var a = 12;
+// console.log(a); 
+
+// let a = 34;
+// let a = 12;
+// console.log(a);
+// Uncaught SyntaxError: Identifier 'a' has already been declared (at 24jan.js:27:5)
+
+
+// let q = 20;
+
+// {let q = 12;
+//   console.log(q); // 12 
+// }
+
+// console.log(q); // 20
+
+
+// function add(a,b){
+// return a+b;
+// }
+// console.log(add(2,3));
+ 
+
+// const a;
+// console.log(a);
+
+
+
+
