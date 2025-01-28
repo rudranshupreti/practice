fd5ca3d7d1507e54f6fc7ead20a3f86819b0c6ab
rudranshupreti17@gmail.com <rudranshupreti7@gmail.com>
2025-01-28 11:16:06 +0530

    inital commit

---
+++ String.js
@@ -0,0 +1,79 @@
+// string is use to store the data in or text it is use to writ with quotes
+
+// let text = "rudranh upreti"
+// console.log(text);
+// string double or single quot me sam ekam krti he 
+
+// let lenght = text.length;
+// console.log(lenght);
+
+// let person = "hello sir me name is \"udran\"esyhseet.";
+// let person1 = "hewjifbwbfaw\\qewegqewg"
+// console.log(person)
+
+// let x = "john";
+// let y =  new String("john");
+// a = x ===y ;
+// console.log(a);//true
+
+
+
+// we have 4 tyep to extract charactor
+// let name = "rudrasnh";
+// let char =  name.charAt(3);
+// let charCode = name.charCodeAt(3);
+// let letter =  name.at(4);
+// let braket = name[2];
+
+// console.log(char);
+// console.log(letter);
+// console.log(charCode);
+// console.log(braket);
+
+// we have three method to extract string part
+ 
+// let name = "rudransh wfwE2  ERW2R23QR2QR132RE";
+// let part =  name.slice(3,6);
+// console.log(part);
+ 
+// let part2 = name.slice(5);
+// console.log(part2)
+// let Part3 = name.slice (-5,-1)
+// console.log(Part3);
+
+// let name = "    I am   gooD boy    rudransh.     "
+
+// const result = "I Am Good Boy Rudransh."
+
+// function string (name){
+//   let outerspace = name.split(" ");
+  
+//   let final = [];
+//   for (const word of outerspace){
+//     if (
+//       word.length >=1
+//     ){
+//       final.push(word.charAt(0).toUpperCase()+word.slice(1).toLowerCase())
+//     }
+//   }
+
+//   return final.join(" ")
+  
+  
+// }
+
+
+// console.log(string(name));
+
+// let text =  `he's name is "prakhar" `;
+// console.log(text);
+// using substring ye slice jese kaam krta diffrence itna he bss isme ham value 0 nhi kr skte end or starting ki 
+
+let str = " aiwkbfiwbgf";
+let text = str.substring(2,4);
+console.log(text);
+
+//substr ye slice ki jese kaam karta he bss insme phele value se jitne index value ko start krta he or end last value me krta he 
+let str2 = str.substr(3,6);
+console.log(str2);
+
